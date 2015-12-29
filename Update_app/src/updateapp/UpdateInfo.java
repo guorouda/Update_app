eb3882d895d5490e864ab155a311232f5f06b7da
sunrong <sunrong@msn.com>
2015-12-29 16:55:21 +0800

    server ok & autoupdate ok

--- Update_app/src/updateapp/UpdateInfo.java
+++ Update_app/src/updateapp/UpdateInfo.java
@@ -9,13 +9,21 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.net.MalformedURLException;
 
+import javax.servlet.ServletException;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+
+import org.apache.catalina.LifecycleException;
+
+import com.ron.Main;
 
 /**
  *
@@ -23,7 +31,12 @@ import javax.swing.JScrollPane;
  */
 public class UpdateInfo extends JFrame{
 
-    private JEditorPane infoPane;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JEditorPane infoPane;
     private JScrollPane scp;
     private JButton ok;
     private JButton start;
@@ -34,12 +47,43 @@ public class UpdateInfo extends JFrame{
     public UpdateInfo() {
         initComponents();
 //        infoPane.setText(info);
+        infoPane.setText("Tomcat Server starting...");
+    	new Thread(new Runnable() {
+		    public void run() {
+		        // TODO Auto-generated method stub
+		    	try {
+					 Main.start();
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+//					e.printStackTrace();
+					infoPane.setText("Tomcat Server start error!!");
+				} catch (ServletException e) {
+					// TODO Auto-generated catch block
+//					e.printStackTrace();
+					infoPane.setText("Tomcat Server start error!!");
+				} catch (LifecycleException e) {
+					// TODO Auto-generated catch block
+//					e.printStackTrace();
+					infoPane.setText("Tomcat Server start error!!");
+				} catch(Exception e){							
+					System.out.println("erro!");
+				}
+		    }
+		}).start();
+    	
+		System.out.println("started");
+		infoPane.setText("Tomcat Server started");
     }
-
+    
     private void initComponents() {
 
         this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        this.setTitle("New Update Found v2.0");
+        //this.setDefaultCloseOperation(0);//点击右上角,DO_NOTHING_ON_CLOSE，不执行任何操作。  
+        //this.setDefaultCloseOperation(1);//HIDE_ON_CLOSE，只隐藏界面，setVisible(false)  
+        //this.setDefaultCloseOperation(2);//DISPOSE_ON_CLOSE,隐藏并释放窗体。当最后一个窗口被释放后，则程序也随之运行结束。  
+        //this.setDefaultCloseOperation(3);//EXIT_ON_CLOSE,直接关闭应用程序System.exit(0);  
+        
+        this.setTitle("New Update Found ");
         pan1 = new JPanel();
         pan1.setLayout(new BorderLayout());
 
@@ -56,8 +100,32 @@ public class UpdateInfo extends JFrame{
         start.addActionListener( new ActionListener(){
 
             public void actionPerformed(ActionEvent e) {
-                         	 
-	           infoPane.setText("Tomcat Server starting...");
+	            infoPane.setText("Tomcat Server starting...");
+            	new Thread(new Runnable() {
+				    public void run() {
+				        // TODO Auto-generated method stub
+				    	try {
+							 Main.start();
+						} catch (MalformedURLException e) {
+							// TODO Auto-generated catch block
+//							e.printStackTrace();
+							infoPane.setText("Tomcat Server start error!!");
+						} catch (ServletException e) {
+							// TODO Auto-generated catch block
+//							e.printStackTrace();
+							infoPane.setText("Tomcat Server start error!!");
+						} catch (LifecycleException e) {
+							// TODO Auto-generated catch block
+//							e.printStackTrace();
+							infoPane.setText("Tomcat Server start error!!");
+						} catch(Exception e){							
+							System.out.println("erro!");
+						}
+				    }
+				}).start();
+            	
+				System.out.println("started");
+				infoPane.setText("Tomcat Server started");
             }
         });
         
@@ -88,6 +156,22 @@ public class UpdateInfo extends JFrame{
         cancel.addActionListener( new ActionListener(){
 
             public void actionPerformed(ActionEvent e) {
+            	
+            	new Thread(new Runnable() {
+				    public void run() {
+				        // TODO Auto-generated method stub
+				    	try {
+							 Main.stop();
+							 System.out.println("stopped");
+							 infoPane.setText("Tomcat Server stopped");
+							 
+						} catch (LifecycleException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				    }
+				}).start();
+            	
                 UpdateInfo.this.dispose();
             }
         });
@@ -99,9 +183,31 @@ public class UpdateInfo extends JFrame{
         pan1.add(pan2, BorderLayout.SOUTH);
         pan1.add(scp, BorderLayout.CENTER);
         this.add(pan1);
-        pack();
-        show();
+        this.pack();
+        this.setVisible(true); 
         this.setSize(300, 200);
+        
+
+        this.addWindowListener(new WindowAdapter() {
+        	public void windowClosing(WindowEvent e) {
+        		new Thread(new Runnable() {
+				    public void run() {
+				        // TODO Auto-generated method stub
+				    	try {
+							 Main.stop();
+							 System.out.println("stopped");
+							 infoPane.setText("Tomcat Server stopped");
+							 
+						} catch (LifecycleException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				    }
+				}).start();
+        		System.exit(0);
+        	}
+        });
+
     }
     private void update()
     {
